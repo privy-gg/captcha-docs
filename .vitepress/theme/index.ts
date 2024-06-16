99d8b6809ea23b23c53e8a12ae80fc87307e1696
ImDarkDiamond <dark@arcane.bot>
2024-06-16 16:13:24 -0600

    image zooming

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,17 +1,34 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import type { Theme } from 'vitepress'
+import { h, nextTick, onMounted, watch } from 'vue'
+import { useRoute, type Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import mediumZoom from 'medium-zoom'
 
 export default {
-  extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
+    extends: DefaultTheme,
+    Layout: () => {
+      return h(DefaultTheme.Layout, null, {
+        // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      })
+    },
+    enhanceApp({ app, router, siteData }) {},
+
+    // Image zooming: https://github.com/vuejs/vitepress/issues/854
+    setup() {
+        const route = useRoute();
+        const initZoom = () => {
+            // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); 
+            mediumZoom('.VPDoc img', { background: 'var(--vp-c-bg)' });
+        };
+   
+        onMounted(() => {
+            initZoom();
+        });
+        
+        watch(
+            () => route.path,
+            () => nextTick(() => initZoom())
+        );
   },
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
 } satisfies Theme
